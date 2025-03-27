a48c30dcbdcc7b47d4c9748f4df927272c9a3a4a
Raj Jain <onefineday0105@gmail.com>
2025-03-28 04:17:42 +0530

    onefineday01/shortify

---
+++ src/main/java/com/onefineday01/shortify/ShortifyApplication.java
@@ -0,0 +1,13 @@
+package com.onefineday01.shortify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortifyApplication.class, args);
+	}
+
+}
